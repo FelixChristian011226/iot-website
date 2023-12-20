01f199d6301b16afe97938084918fe257a140ac4
wangyuefei <1930552052@qq.com>
2023-12-20 22:58:51 +0800

    Initialize backend of the iot website.

---
+++ iot-backend/src/main/java/com/felix/iotbackend/IotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.felix.iotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotBackendApplication.class, args);
+	}
+
+}
---
+++ iot-backend/src/test/java/com/felix/iotbackend/IotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felix.iotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
