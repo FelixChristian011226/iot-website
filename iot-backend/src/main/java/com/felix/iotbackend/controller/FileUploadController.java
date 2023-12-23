fb83dc8d5bd204f75c46d32597efc95ab3badcc5
wangyuefei <1930552052@qq.com>
2023-12-23 20:19:42 +0800

    Add local file store function.

---
+++ iot-backend/src/main/java/com/felix/iotbackend/controller/FileUploadController.java
@@ -0,0 +1,25 @@
+package com.felix.iotbackend.controller;
+
+import com.felix.iotbackend.pojo.Result;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+public class FileUploadController {
+
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) throws IOException {
+        //SAVE FILE TO LOCAL DISK
+        String originalFilename = file.getOriginalFilename();
+        String filename = UUID.randomUUID().toString()+originalFilename.substring(originalFilename.lastIndexOf("."));
+        String property = System.getProperty("user.dir");   //GET CURRENT WORKING DIRECTORY
+        String path = (new File(property)).getParent()+File.separator+"iot-database"+File.separator+"files";    //.\iot-website\iot-database\files
+        file.transferTo(new File(path+File.separator+filename));
+        return Result.success(path+File.separator+filename);
+    }
+}
