c8e660d319e0abc36900ac2c617abd8a414ffb93
wangyuefei <1930552052@qq.com>
2023-12-26 03:18:14 +0800

    Fixed login bug.

--- iot-frontend/vue-project/src/App.vue
+++ iot-frontend/vue-project/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import LoginVue from '@/views/Login.vue'
+  import LayoutVue from '@/views/Layout.vue'
 </script>
 
 <template>
   <LoginVue/>
+  <LayoutVue/>
 </template>
 
 <style scoped>
